6e2543cc6ad759019733fe0480d11f7a3c280213
yuwei <shinyhz50817>
2016-05-05 11:40:34 +0800

    新增插件
    优化css压缩，添加css url文件的版本号插件

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@
  */
 // 加载插件
 var gulp = require('gulp'),
-    minifycss = require('gulp-minify-css'), //压缩css插件
+    cleanCSS = require('gulp-clean-css'), //压缩css插件
     uglify = require('gulp-uglify'), //压缩js插件
     imagemin = require('gulp-imagemin'),    //压缩图片插件
     notify = require('gulp-notify'),    //更改提醒插件
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
     sass = require('gulp-ruby-sass'),   //编译scss插件
+    cssver = require('gulp-make-css-url-version'),//给css文件中引用文件加版本号(md5)
 //处理后存放的位置
     cssPath = "dist/css",
     jsPath = 'dist/js',
@@ -18,9 +19,13 @@ var gulp = require('gulp'),
 // 压缩CSS
 gulp.task('css', function () {
     return gulp.src('css/*.css')
-        .pipe(minifycss())
+        .pipe(cssver()) //给css文件中引用文件加版本号(md5)
+        .pipe(cleanCSS({compatibility: 'ie8', debug: true}, function (details) {
+            console.log(details.name + 'before:' + details.stats.originalSize); //打印压缩前size
+            console.log(details.name + 'after:' + details.stats.minifiedSize); //打印压缩后size
+        }))
         .pipe(gulp.dest(cssPath))
-        .pipe(notify({message: 'Styles task complete'}));
+        .pipe(notify({message: 'css ok'}));
 });
 
 // 压缩js
@@ -28,7 +33,7 @@ gulp.task('js', function () {
     return gulp.src('js/*.js')
         .pipe(uglify())
         .pipe(gulp.dest(jsPath))
-        .pipe(notify({message: 'Scripts task complete'}));
+        .pipe(notify({message: 'js ok'}));
 });
 
 // 压缩图片
@@ -37,7 +42,7 @@ gulp.task('images', function () {
         .pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
         .pipe(gulp.dest(imagePath))
         //提醒任务完成
-        .pipe(notify({message: 'Images task complete'}));
+        .pipe(notify({message: 'Images ok'}));
 });
 
 //编译sass
